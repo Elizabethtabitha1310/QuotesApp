88a4fb05966c8114834ecee04a8d965d036b3a7e
elizabethtabitha1310 <tabyeliza@gmail.com>
2024-09-02 12:43:02 +0300

    updated quotes activity

--- app/src/main/java/com/example/quotesapp/ui/quotes/QuotesActivity.kt
+++ app/src/main/java/com/example/quotesapp/ui/quotes/QuotesActivity.kt
@@ -1,21 +1,45 @@
 package com.example.quotesapp.ui.quotes
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.quotesapp.R
+import com.example.quotesapp.data.Quote
+import com.example.quotesapp.utilities.InjectorUtils
 
 class QuotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_quotes)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        initializeUi()
     }
+private  fun initializeUi(){
+    val factory = InjectorUtils.provideQuotesViewModelFactory()
+    val viewModel = ViewModelProviders.of(this, factory)
+        .get(QuotesViewModel::class.java)
+
+    val textViewQuotes = findViewById<TextView>(R.id.textView_quotes)
+    val buttonAddQuote = findViewById<Button>(R.id.button_add_quote)
+    val editTextQuote = findViewById<EditText>(R.id.editText_quote)
+    val editTextAuthor = findViewById<EditText>(R.id.editText_author)
+
+    viewModel.getQuotes().observe(this, Observer { quotes ->
+        val stringBuilder = StringBuilder()
+        quotes.forEach { quote ->
+            stringBuilder.append("$quote\n\n")
+        }
+        textViewQuotes.text = stringBuilder.toString()
+    })
+
+    buttonAddQuote.setOnClickListener {
+        val quote = Quote(editTextQuote.text.toString(), editTextAuthor.text.toString())
+        viewModel.addQuotes(quote)
+        editTextQuote.setText("")
+        editTextAuthor.setText("")
+    }
+  }
 }
