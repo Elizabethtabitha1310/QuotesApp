dd8d1b7f220a3fd1f73e9d6338d73d08fc23e241
elizabethtabitha1310 <tabyeliza@gmail.com>
2024-09-02 11:44:37 +0300

    quotes viewmodel factory

--- app/src/main/java/com/example/quotesapp/ui/quotes/QuotesViewModel.kt
+++ app/src/main/java/com/example/quotesapp/ui/quotes/QuotesViewModel.kt
@@ -5,7 +5,9 @@ import com.example.quotesapp.data.Quote
 import com.example.quotesapp.data.QuoteRepository
 
 class QuotesViewModel (private val quoteRepository: QuoteRepository):ViewModel(){
+
     fun getQuotes() = quoteRepository.getQuotes()
+
     fun addQuotes(quote: Quote) = quoteRepository.addQuote(quote)
 
 }
